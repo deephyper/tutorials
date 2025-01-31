cbccfa5591354f26ac487a7ee3eaab5615d30cf4
Deathn0t <romainegele@gmail.com>
2025-01-31 16:10:28 +0100

    removing outdated tutorials

--- tutorials/scripts/07_HPS_with_Data_Parallel_Training/dh_project/dh_project/mnist/problem.py
+++
@@ -1,9 +0,0 @@
-from deephyper.hpo import HpProblem
-
-Problem = HpProblem()
-
-Problem.add_hyperparameter((1e-4, 1e-1, "log-uniform"), "lr")
-
-
-if __name__ == "__main__":
-    print(Problem)
--- tutorials/scripts/12_NAS_LSTM/dh_project/dh_project/lstm_search/download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget "https://github.com/deephyper/tutorials/blob/main/tutorials/12_NAS_LSTM/dh_project/dh_project/lstm_search/True_Train.npy?raw=true" -O True_Train.npy
-wget "https://github.com/deephyper/tutorials/blob/main/tutorials/12_NAS_LSTM/dh_project/dh_project/lstm_search/True_Test.npy?raw=true" -O True_Test.npy
